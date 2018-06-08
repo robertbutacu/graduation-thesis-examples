15051999cb368dae04da72e47d65e69740520605
robertbutacu <robertbutacu@gmail.com>
2018-06-09 01:15:08 +0300

    layers

---
+++ scala/examples/src/main/scala/layers/Layers.scala
@@ -0,0 +1,38 @@
+package layers
+
+import java.io.IOException
+
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
+
+object Layers extends App {
+  def count(file: String): Unit = {
+    val content = Source.fromFile(file).getLines.toList
+
+    val words = content.flatMap(line => line.split(" "))
+    val counter = words.groupBy(w => w).values.map(w => (w.head, w.length))
+    print(counter)
+  }
+
+  def readFromFile(file: String): Try[List[String]] = Try {
+    Source.fromFile(file).getLines().toList
+  }
+
+  def getWords = (lines: List[String]) => lines.flatMap(line => line.split(" "))
+  def groupWords = (words: List[String]) => words.groupBy(w => w).values.toList
+  def countWords = (groupedWords: List[List[String]]) => groupedWords.map(g => (g.head, g.length))
+  def prettyPrinter(wordsCount: List[(String, Int)]): Unit =
+    wordsCount.foreach(word => println(s"""Word: ${word._1} Count: ${word._2}"""))
+
+  def countFromFile(file: String): Unit = {
+    def counter = getWords andThen groupWords andThen countWords andThen prettyPrinter
+
+    readFromFile(file) match {
+      case Success(lines) => counter(lines)
+      case Failure(ex) => println(ex)
+    }
+  }
+
+  countFromFile("resources\\test.txt")
+}
