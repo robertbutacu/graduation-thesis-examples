e9473ffc1829895c140447f09cab5296a73ba915
robertbutacu <robertbutacu@gmail.com>
2018-06-15 14:47:26 +0300

    [bug] logic fixed

--- scala/examples/src/main/scala/design/patterns/monads/MonadsExample.scala
+++ scala/examples/src/main/scala/design/patterns/monads/MonadsExample.scala
@@ -2,9 +2,9 @@ package design.patterns.monads
 
 object MonadsExample extends App {
   def division(a: Double)(b: Double): Option[Double] = {
-    if(b == 0.0)
+    if (b == 0.0)
       None
-    else Some(b)
+    else Some(a / b)
   }
 
   val divisionOf10 = division(10)(_)
